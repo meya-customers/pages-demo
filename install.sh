049f920f3201898aa84159e9fce4152078a01291
Dan Cecile <dan@meya.ai>
2021-07-08 17:04:48 -0400

    Handle assign event

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-echo "Installing custom requirements"
-apk add --no-cache --virtual .build-deps \
-  gcc \
-  python3-dev \
-&& pip install --disable-pip-version-check --no-cache-dir -r requirements.txt
